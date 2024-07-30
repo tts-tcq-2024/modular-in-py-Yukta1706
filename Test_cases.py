560e869f047fb77b3a3b8918c2fc99486b8b48d1
Yukta1706 <75121611+Yukta1706@users.noreply.github.com>
2024-07-30 16:35:56 +0530

    Renaming_to_test_cases

--- Test_cases.py
+++ Test_cases.py
@@ -1,4 +1,4 @@
-from Different_colours import get_color_from_pair_number,get_pair_number_from_color
+from Colors import get_color_from_pair_number,get_pair_number_from_color
  
 def test_number_to_pair(pair_number,
                         expected_major_color, expected_minor_color):
